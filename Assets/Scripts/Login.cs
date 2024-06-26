ecd170009d1a11335cbd89c2b3d0ea2b6187e1a3
ozingozing <hoon4367@gmail.com>
2024-06-26 20:51:56 +0900

    [FIX] Login Craete 분리

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -6,20 +6,31 @@ using UnityEngine.UI;
 
 public class Login : MonoBehaviour
 {
-	[SerializeField] private string authenticationEndpoint = "http://127.0.0.1:13756/account";
+	[SerializeField] private string loginEndpoint = "http://127.0.0.1:13756/account/login";
+	[SerializeField] private string createEndpoint = "http://127.0.0.1:13756/account/create";
 
 	[SerializeField] private TextMeshProUGUI alertText;
 	[SerializeField] private Button loginButton;
-    [SerializeField] private TMP_InputField usernameInputField;
+	[SerializeField] private Button createButton;
+	[SerializeField] private TMP_InputField usernameInputField;
 	[SerializeField] private TMP_InputField passwordInputField;
 
 	public void OnLoginClick()
 	{
 		alertText.text = "Singin in...";
-		loginButton.interactable = false;
+		ActivateButtons(false);
+
 		StartCoroutine(TryLogin());
 	}
 
+	public void OnCraeteClick()
+	{
+		alertText.text = "Creating account...";
+		ActivateButtons(false);
+
+		StartCoroutine(TryCreate());
+	}
+	
 	private IEnumerator TryLogin()
 	{
 
@@ -42,7 +53,11 @@ public class Login : MonoBehaviour
 			yield break;
 		}
 
-		UnityWebRequest request = UnityWebRequest.Get($"{authenticationEndpoint}?rUsername={username}&rPassword={password}");
+		WWWForm form = new WWWForm();
+		form.AddField("rUsername", username);
+		form.AddField("rPassword", password);
+
+		UnityWebRequest request = UnityWebRequest.Post(loginEndpoint, form);
 		var handler = request.SendWebRequest();
 
 		float startTime = 0.0f;
@@ -62,7 +77,7 @@ public class Login : MonoBehaviour
 		{
 			if(request.downloadHandler.text != "Invalid credentials") //login success?
 			{
-				loginButton.interactable = false;
+				ActivateButtons(false);
 				GameAccount returnedAccount =
 					JsonUtility.FromJson<GameAccount>(request.downloadHandler.text);
 				alertText.text = "Welcome "
@@ -72,15 +87,83 @@ public class Login : MonoBehaviour
 			else
 			{
 				alertText.text = "Invalid credentials";
-				loginButton.interactable = true;
+				ActivateButtons(true);
 			}
 		}
 		else
 		{
 			alertText.text = "Error connecting to the server...";
-			loginButton.interactable = true;
+			ActivateButtons(true);
 		}
 
 		yield return null;
+	}
+
+	private IEnumerator TryCreate()
+	{
+
+		string username = usernameInputField.text;
+		string password = passwordInputField.text;
+
+		if (username.Length < 3 || username.Length > 24)
+		{
+			alertText.text = "Invalid username";
+			ActivateButtons(true);
+
+			yield break;
+		}
+
+		if (password.Length < 3 || password.Length > 24)
+		{
+			alertText.text = "Invalid password";
+			ActivateButtons(true);
+
+			yield break;
+		}
+
+		WWWForm form = new WWWForm();
+		form.AddField("rUsername", username);
+		form.AddField("rPassword", password);
+
+		UnityWebRequest request = UnityWebRequest.Post(createEndpoint, form);
+		var handler = request.SendWebRequest();
+
+		float startTime = 0.0f;
+		while (!handler.isDone)
+		{
+			startTime += Time.deltaTime;
+
+			if (startTime > 10.0f)
+			{
+				break;
+			}
+
+			yield return null;
+		}
+
+		if (request.result == UnityWebRequest.Result.Success)
+		{
+			if (request.downloadHandler.text != "Invalid credentials" 
+				&& request.downloadHandler.text != "Username is already taken") //login success?
+			{
+				GameAccount returnedAccount =
+					JsonUtility.FromJson<GameAccount>(request.downloadHandler.text);
+				alertText.text = "Account has been created";
+			}
+			else
+				alertText.text = "Username is already taken";
+		}
+		else
+			alertText.text = "Error connecting to the server...";
+
+		ActivateButtons(true);
+
+		yield return null;
+	}
+
+	private void ActivateButtons(bool toggle)
+	{
+		loginButton.interactable = toggle;
+		createButton.interactable = toggle;
 	}
 }
